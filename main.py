dda8c152dafd96bcb1732b30d7b68d8ca7688cf8
Sunchock <toto.marestin@gmail.com>
2022-03-06 21:43:05 +0100

    Reset repository, keep only code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import discord
 from discord.ext import commands
@@ -8,7 +10,7 @@ load_dotenv(dotenv_path='.config')
 class TchoupiBot(commands.Bot):
 	def __init__(self):
 		default_intents = discord.Intents.default()
-		default_intents.members = True
+		default_intents.members = True 
 		super().__init__(command_prefix="!tchoupi ", intents=default_intents)
 
 		@self.command(name='test')
@@ -16,7 +18,26 @@ class TchoupiBot(commands.Bot):
 			admin_chanel: discord.TextChannel = self.get_channel(705763034295173120)
 			await admin_chanel.send(content="Liste des commandes :")
 			await admin_chanel.send(content="Aucune ! Le bot est en travaux dsl")
-			print("call tchoupi's help")
+		
+		@self.command(name="hello")
+		async def hello(ctx):
+			welcome_channel = self.get_channel(853383864713871390)
+			await welcome_channel.purge()
+			await ctx.channel.purge()
+			await ctx.send("""Bonjour ! Bienvenue sur le discord de la résidence Daniel Faucher :partying_face: !
+
+				Pour avoir accès aux autres salons de ce serveur, tu dois d'abord lire et accepter les règles !
+
+				1 : Soyez aimable et courtois
+				2 : Pas d'incitation à la haine, ni de harcèlement
+				3 : Respectez la confidentialité de tous
+				4 : Pas de promotions idéologiques/commerciales
+
+				Approuve les règles et obtiens tes accès avec l'émoji qui correspond au numéro de ton bâtiment :slight_smile: 
+
+				Bât 1 : :one:  | Bât 2 : :two:  | Bât 3 : :three:  | Bât 4 : :four:  | Bât 5 : :five:  | Bât 6 : :six:  | Bât 7 : :seven:  | Bât 8 : :eight:
+				
+				En poursuivant ta navigation sur ce serveur, tu reconnais avoir pris connaissance et approuvé les règles énnoncées ci-dessus.""")
 
 	async def on_ready(self):
 		print(f"{self.user.display_name} est connecté au serveur.")
