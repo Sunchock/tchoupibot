7f17d226f25a011d61af7cea0f8417625291b067
Sunchock <toto.marestin@gmail.com>
2023-01-16 00:57:53 +0100

    Add debug logs

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,6 +5,7 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(0, os.path.dirname(__file__) + '/src')
+print(sys.path) # DEBUG
 
 wsgi = imp.load_source('wsgi', 'src/main.py')
 application = wsgi.tchoupibot
