7527de8cb567647c7107a51ff1ff70be92aa4b42
Sunchock <toto.marestin@gmail.com>
2023-01-18 22:06:33 +0100

    Add debug logs

--- src/tchoupibot.py
+++ src/tchoupibot.py
@@ -28,10 +28,10 @@ def tchoupibot(environ, start_response):
 
 # Entry point
 if __name__ == "__main__":
-	pgrep_proc = subprocess.Popen(['pgrep -f .*tchoupibot.*'], shell=True, stdout=subprocess.PIPE)
+	pgrep_proc = subprocess.Popen(['pgrep -af .*tchoupibot.*'], shell=True, stdout=subprocess.PIPE)
 	processes: list[bytes] = pgrep_proc.communicate("")[0].splitlines()
 
-	print(processes) # DEBUG
+	print(processes, os.getpid(), pgrep_proc.pid) # DEBUG
 	processes.remove(str(os.getpid()).encode())
 	processes.remove(str(pgrep_proc.pid).encode())
 	if processes:
