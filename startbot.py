f93aff7b2edd0206e7bf45581ccf5f9f82318e49
Sunchock <toto.marestin@gmail.com>
2023-01-19 10:11:51 +0100

    DEBUG CRON #10 remove debug logs

--- startbot.py
+++ startbot.py
@@ -5,16 +5,11 @@ import subprocess
 pgrep_proc = subprocess.Popen(['pgrep -af .*tchoupibot.py'], shell=True, stdout=subprocess.PIPE)
 processes: list[bytes] = pgrep_proc.communicate("")[0].splitlines()
 
-print(subprocess.check_output(['pgrep -f .*tchoupibot.py'], shell=True))
-
 current_pid = str(os.getpid()).encode()
-
-print(processes, current_pid) # DEBUG
 if current_pid in processes:
 	processes.remove(current_pid)
-print(processes, current_pid) # DEBUG
 if processes:
 	print("Bot already running.")
 	exit(1)
-bot_process = subprocess.run("python src/tchoupibot.py")
+subprocess.run("python src/tchoupibot.py")
 exit(0)
