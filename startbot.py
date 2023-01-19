1b0a30353e6e170f26ca69864fcee8fa6afad627
Sunchock <toto.marestin@gmail.com>
2023-01-19 01:07:16 +0100

    DEBUG CRON #9

--- startbot.py
+++ startbot.py
@@ -2,21 +2,19 @@
 import os
 import subprocess
 
-# Entry point
-if __name__ == "__main__":
-	pgrep_proc = subprocess.Popen(['pgrep -af .*tchoupibot.py'], shell=True, stdout=subprocess.PIPE)
-	processes: list[bytes] = pgrep_proc.communicate("")[0].splitlines()
+pgrep_proc = subprocess.Popen(['pgrep -af .*tchoupibot.py'], shell=True, stdout=subprocess.PIPE)
+processes: list[bytes] = pgrep_proc.communicate("")[0].splitlines()
 
-	print(subprocess.check_output(['pgrep -f .*tchoupibot.py'], shell=True))
+print(subprocess.check_output(['pgrep -f .*tchoupibot.py'], shell=True))
 
-	current_pid = str(os.getpid()).encode()
+current_pid = str(os.getpid()).encode()
 
-	print(processes, current_pid) # DEBUG
-	if current_pid in processes:
-		processes.remove(current_pid)
-	print(processes, current_pid) # DEBUG
-	if processes:
-		print("Bot already running.")
-		exit(1)
-	bot_process = subprocess.run("python src/tchoupibot.py")
-	exit(0)
+print(processes, current_pid) # DEBUG
+if current_pid in processes:
+	processes.remove(current_pid)
+print(processes, current_pid) # DEBUG
+if processes:
+	print("Bot already running.")
+	exit(1)
+bot_process = subprocess.run("python src/tchoupibot.py")
+exit(0)
